c9eb8cea77f1e4ad2eff4c4b323af62c21579d06
Rauof M <abdelrauof197@gmail.com>
2018-02-08 01:16:40 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) AS total_tardies_for_all_students FROM students;"
+  "SELECT SUM(tardies) AS total_tardies_for_all_students WHERE NAME=9;"
 end
 
 def average_gpa_for_9th_grade
